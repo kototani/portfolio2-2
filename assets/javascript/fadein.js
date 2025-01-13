0a5137f3ff9178991442a9c0208e3b5602f6a0e9
kototani <torayuki55_kainue@icloud.com>
2025-01-13 23:59:30 +0900

    Add files via upload

---
+++ assets/javascript/fadein.js
@@ -0,0 +1,59 @@
+$(function () {
+  $(window).scroll(function () {
+    $(".small-head").each(function () {
+      var elemPos = $(this).offset().top; /* 要素の位置を取得 */
+      var scroll = $(window).scrollTop(); /* スクロール位置を取得 */
+      var windowHeight = $(window).height(); /* 画面幅を取得（画面の下側に入ったときに動作させるため） */
+      if (scroll > elemPos - windowHeight) {
+        /* 要素位置までスクロール出来たときに動作する */
+        $(this).addClass("effect-scroll");
+      }
+    });
+    $(".news-contents-list-item").each(function () {
+      var elemPos = $(this).offset().top; /* 要素の位置を取得 */
+      var scroll = $(window).scrollTop(); /* スクロール位置を取得 */
+      var windowHeight = $(window).height(); /* 画面幅を取得（画面の下側に入ったときに動作させるため） */
+      if (scroll > elemPos - windowHeight) {
+        /* 要素位置までスクロール出来たときに動作する */
+        $(this).addClass("effect-scroll");
+      }
+    });
+    $(".view-more").each(function () {
+      var elemPos = $(this).offset().top; /* 要素の位置を取得 */
+      var scroll = $(window).scrollTop(); /* スクロール位置を取得 */
+      var windowHeight = $(window).height(); /* 画面幅を取得（画面の下側に入ったときに動作させるため） */
+      if (scroll > elemPos - windowHeight) {
+        /* 要素位置までスクロール出来たときに動作する */
+        $(this).addClass("effect-scroll");
+      }
+    });
+    $(".about-text").each(function () {
+      var elemPos = $(this).offset().top; /* 要素の位置を取得 */
+      var scroll = $(window).scrollTop(); /* スクロール位置を取得 */
+      var windowHeight = $(window).height(); /* 画面幅を取得（画面の下側に入ったときに動作させるため） */
+      if (scroll > elemPos - windowHeight) {
+        /* 要素位置までスクロール出来たときに動作する */
+        $(this).addClass("effect-scroll");
+      }
+    });
+    $(".view-visible").each(function () {
+      var elemPos = $(this).offset().top; /* 要素の位置を取得 */
+      var scroll = $(window).scrollTop(); /* スクロール位置を取得 */
+      var windowHeight = $(window).height(); /* 画面幅を取得（画面の下側に入ったときに動作させるため） */
+      if (scroll > elemPos - windowHeight) {
+        /* 要素位置までスクロール出来たときに動作する */
+        $(this).addClass("effect-scroll");
+      }
+    });
+    $(".footer").each(function () {
+      var elemPos = $(this).offset().top; /* 要素の位置を取得 */
+      var scroll = $(window).scrollTop(); /* スクロール位置を取得 */
+      var windowHeight = $(window).height(); /* 画面幅を取得（画面の下側に入ったときに動作させるため） */
+      if (scroll > elemPos - windowHeight) {
+        /* 要素位置までスクロール出来たときに動作する */
+        $(this).addClass("effect-scroll");
+      }
+    });
+  });
+  jQuery(window).scroll();
+});
