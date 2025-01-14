8fd9a05b7bf737adb02d158e7d6c9a9ac1baacae
kototani <torayuki55_kainue@icloud.com>
2025-01-15 03:31:00 +0900

    Add files via upload

--- assets/javascript/head-image-slide1.js
+++ assets/javascript/head-image-slide1.js
@@ -1,12 +1,17 @@
-window.addEventListener("load", () => {
-  const infiniteSlider = new Swiper(".infinite-slider", {
-    loop: true,
-    slidesPerView: "1",
-    spaceBetween: 0,
-    speed: 1000,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-  });
+ // それぞれのSwiperインスタンスを個別に初期化
+ const swiper1 = new Swiper(".infinite-slider1", {
+  loop: true,
+  speed: 1000, // スライドのスピード
+  autoplay: { // 自動再生
+    delay: 2000, // 1.5秒後に次のスライド
+  },
+});
+
+const swiper2 = new Swiper(".infinite-slider2", {
+  loop: true,
+  speed: 1000, // スライドのスピード
+  autoplay: { // 自動再生
+    delay: 2100, // 1.5秒後に次のスライド
+  },
+  rtl: false, // 逆方向ではなく左から右にスライド
 });
